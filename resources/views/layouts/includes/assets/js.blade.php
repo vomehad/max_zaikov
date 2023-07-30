7089a13bec960c59eda560a01367827c7fcdca08
DenisMozhaev <d.mozhaev@digital-sector.ru>
2023-07-30 22:59:09 +0300

    max_1 | layouts | js

--- resources/views/layouts/includes/assets/js.blade.php
+++ resources/views/layouts/includes/assets/js.blade.php
@@ -1,2 +1,34 @@
-<script src="{{ mix('/js/app.js') }}"></script>
+{{--<script src="{{ mix('/js/app.js') }}"></script>--}}
+
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/jquery-3.2.1.min.js"></script>
+
+
+<script type="text/javascript" src="https://www.artis21.ru/local/templates/artis21.ru/js/slick/slick.min.js"></script>
+
+<!-- <script src="https://api-maps.yandex.ru/2.1/?apikey=d9edc0a7-0f68-404e-9dea-629f9fbe1126&lang=ru_RU"></script> -->
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/jquery.formstyler.js"></script>
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/jquery.maskedinput.js"></script>
+
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/jQueryUI/jquery-ui.min.js"></script>
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/jQueryUI/jquery.ui.touch-punch.min.js"></script>
+
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/jquery.lazyload.js"></script>
+
+<script src="/js/jquery.form/jquery.form.min.js"></script>
+<script src="/js/jquery.cookie.js"></script>
+
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/jquery.fancybox.min.js"></script>
+<script src="https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+<script src="https://yastatic.net/share2/share.js"></script>
+<!--[if (gt IE 9)|!(IE)]><!-->
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/vendor/svgxuse.min.js" defer></script>
+<![endif]-->
+
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.1/js/OverlayScrollbars.min.js" integrity="sha512-B1xv1CqZlvaOobTbSiJWbRO2iM0iii3wQ/LWnXWJJxKfvIRRJa910sVmyZeOrvI854sLDsFCuFHh4urASj+qgw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>-->
+
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/OverlayScrollbars.js"></script>
+<script src="https://www.artis21.ru/local/templates/.default/project.js"></script>
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/main.js?v=2"></script>
+<script src="https://www.artis21.ru/local/templates/artis21.ru/js/ingevents.4.0.9.min.js"></script>
+
 @stack('scripts')
