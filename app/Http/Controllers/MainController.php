acfe8778d12742d2b27e7deee3265618a553ea72
DenisMozhaev <d.mozhaev@digital-sector.ru>
2023-07-30 22:50:43 +0300

    max_1 | klsdf

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,6 @@ class MainController extends Controller
 {
     public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        dd('gh');
         return view('main.index');
     }
 }
